bf67f3d1e08b45dfa5508dd458309d21b99c8b33
Alex Bannon <alexbannon@gmail.com>
2020-08-20 22:40:15 -0400

    initial commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 34.1;
+            double y = 12.5;
+
+            double result = x + y;
+
+            Console.WriteLine(result);
+
+            if (args.Length > 0) 
+            {
+                Console.WriteLine("Hello " + args[0] + "!"); // concatenation
+                Console.WriteLine($"Hello {args[0]}!"); // interpolation
+            }
+            else 
+            {
+                Console.WriteLine("No Argument Passed!");
+            }            
+        }
+    }
+}
